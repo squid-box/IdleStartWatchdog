5f2106228518b1fe866e391c4db4c27ba5316486
Joel Ahlgren <erik.joel.ahlgren@gmail.com>
2017-01-07 21:47:59 +0100

    Added "automatic" version incrementing, and linked version to installer.

--- IdleStartWatchdog/Properties/AssemblyInfo.cs
+++ IdleStartWatchdog/Properties/AssemblyInfo.cs
@@ -5,13 +5,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IdleStartWatchdog")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Watchdog to shut down computer started by cats.")]
+[assembly: AssemblyCompany("Joel Ahlgren")]
 [assembly: AssemblyProduct("IdleStartWatchdog")]
 [assembly: AssemblyCopyright("Copyright © Joel Ahlgren 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +28,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
